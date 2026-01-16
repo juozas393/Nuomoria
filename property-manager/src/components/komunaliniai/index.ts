e31274ebe0af61dca3457f5773b4a26dfbe7bb91
juozas393 <juozaskazukauskas501@gmail.com>
2026-01-16 14:19:29 +0200

    refactor: complete property-manager to apps/web migration
    - Removed all old property-manager files (now in apps/web)
    - All source code successfully moved to apps/web/
    - Git history preserved for renamed files

--- property-manager/src/components/komunaliniai/index.ts
+++
@@ -1,6 +0,0 @@
-export { TenantMetersModal } from './TenantMetersModal';
-export { MeterRow } from './MeterRow';
-export { FilterChips } from './FilterChips';
-export { useMetersFilters } from './useMetersFilters';
-export type { Meter, MeterUnit, MeterGroup, MeterStatus } from './TenantMetersModal';
-export type { FilterType } from './FilterChips';
--- property-manager/src/components/nuomotojas2/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as TenantList } from './TenantListOptimized';
-export { default as ApartmentCard } from './ApartmentCard';
-export { TenantOverviewPro } from './TenantOverviewPro';
-export { default as TenantDetailModalPro } from './TenantDetailModalPro';
-// TenantOverviewProDemo removed 
